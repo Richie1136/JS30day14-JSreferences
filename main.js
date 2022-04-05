d17c9564bb313f2944d99c82161b4a556b0c28f7
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-05 14:53:27 -0400

    Finished JS30 day14

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 // start with strings, numbers and booleans
 
+let age = 100
+let age2 = age
+
+console.log(age, age2)
+
+age = 200
+
+console.log(age, age2)
+
+let name = 'Robert'
+let name2 = name
+
+console.log(name, name2)
+
+name = 'Joe the go'
+console.log(name, name2)
+
 // Let's say we have an array
 
-const players = ['Wes', 'Sarah', 'Ryan', 'Poppy']
+const players = ['Wes', 'Sarah', 'Ryan', 'Poppy', 'Miker']
 
 // and we want to make a copy of it.
+
+const team = [].concat(players)
+console.log(players)
+console.log(team)
 
 // You might think we can just do something like this:
 
@@ -19,11 +40,17 @@ const players = ['Wes', 'Sarah', 'Ryan', 'Poppy']
 
 // So, how do we fix this? We take a copy instead!
 
+const team2 = players.slice()
+
 // one way
 
 // or create a new array and concat the old one in
 
 // or use the new Es6 Spread
+
+const team3 = [...players]
+
+const team7 = Array.from(players)
 
 // now when we update it, the original one isn't changed
 
@@ -36,7 +63,16 @@ const person = {
   age: 80
 }
 
+console.log(person)
+
+let robot = { ...person }
+robot.age = 90
+console.log(robot)
+
 // and think we make a copy:
+
+let names = Object.assign({}, person, { name: 'Robot' })
+console.log(names)
 
 // how do we take a copy instead?
 
